d8261cc771ad147fd216905632c8405bd37549e1
Julien Letessier <julien.letessier@gmail.com>
2015-01-07 10:32:20 +0100

    Cleanups and comments

--- lib/syslogify/forker.rb
+++ lib/syslogify/forker.rb
@@ -16,21 +16,25 @@ module Syslogify
       @pid = nil
     end
 
+    # causes standard output and error to be redirected to syslog
+    # (through a subprocess)
     def start
-      return if @pid
+      return if @sink
       
-      rd, wr = IO.pipe
+      drain, @sink = IO.pipe
       @old_stdout = STDOUT.dup
       @old_stderr = STDERR.dup
 
+      # Test connection to syslog in the parent process
       Syslog.open(_identity, Syslog::LOG_CONS | Syslog::LOG_NDELAY)
       Syslog.log(Syslog::LOG_NOTICE, 'Diverting logs to syslog')
 
-      @pid = fork do
-        STDIN.reopen(rd)
-        wr.close
+      # spawn a subprocess which pipes from its standard input to syslog
+      pid = fork do
         Process.daemon # otherwise we'll intercept signals
         $PROGRAM_NAME = "#{Syslog.ident}.syslogify"
+        STDIN.reopen(drain)
+        @sink.close
 
         while line = STDIN.gets
           Syslog.log(Syslog::LOG_NOTICE, line.gsub('%', '%%'))
@@ -38,12 +42,13 @@ module Syslogify
         Syslog.log(Syslog::LOG_NOTICE, 'Shutting down')
       end
 
-      Syslog.close
       Process.detach(pid) # the subprocess will shut down on its own once starved of input
+      Syslog.close # the parent does not need syslog access
 
-      rd.close
-      STDOUT.reopen(wr)
-      STDERR.reopen(wr)
+      # redirect stdout/err to the subprocess
+      drain.close
+      STDOUT.reopen(@sink)
+      STDERR.reopen(@sink)
       STDOUT.sync = STDERR.sync = true
      
       begin
@@ -55,15 +60,16 @@ module Syslogify
       self
     end
 
+    # cancels the outout redirection
     def stop
-      return unless @pid
+      return unless @sink
       # NOTE: we shouldn't kill the subprocess (which can be shared igqf the parent
       # forked after #start), and it'll shut down on its own anyways.
       STDOUT.reopen(@old_stdout) unless @old_stdout.closed?
       STDERR.reopen(@old_stderr) unless @old_stderr.closed?
+      @sink.close unless @sink.closed?
       @old_stdout = @old_stderr = nil
-
-      @pid = nil
+      @sink = nil
       self
     end
 
